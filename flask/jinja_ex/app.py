a702217e0ac0c018edb4729419bcc0f9631f4e2c
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-10-30 15:20:53 +0800

    using startup script instead

--- flask/jinja_ex/app.py
+++ flask/jinja_ex/app.py
@@ -4,6 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def template_test():
     return render_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5])
-
-if __name__ == "__main__":
-    app.run(debug=True)
